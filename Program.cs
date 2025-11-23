00dd48285789ecc217518c035991564debfdd347
Dennis Döring <dennis.doering@mobilift.de>
2025-11-23 18:41:10 +0100

    Refactored Auth setup and config
    Moved all auth related setup from Program.cs into separate  extension methods.
    Removed unsued using statements from Program.cs

--- Program.cs
+++ Program.cs
@@ -1,20 +1,11 @@
-using MailArchiver.Auth.Handlers;
-using MailArchiver.Auth.Middlewares;
+using MailArchiver.Auth.Extensions;
 using MailArchiver.Auth.Services;
 using MailArchiver.Data;
 using MailArchiver.Models;
 using MailArchiver.Services;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.DataProtection;
-using Microsoft.AspNetCore.Localization;
-using Microsoft.AspNetCore.RateLimiting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Protocols.OpenIdConnect;
-using System.Data.Common;
-using System.Globalization;
-using System.Security.Claims;
 using System.Threading.RateLimiting;
 
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
@@ -205,64 +196,7 @@ builder.Services.AddRateLimiter(options =>
 });
 
 // Add Authentication
-builder.Services.AddScoped<AuthenticationHandler>();
-builder.Services.AddHttpContextAccessor();
-var authBuilder = builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme);
-authBuilder.AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
-    {
-        options.LoginPath = "/Auth/Login";
-        options.LogoutPath = "/Auth/Logout";
-        options.AccessDeniedPath = "/Auth/AccessDenied";
-        options.Cookie.Name = "MailArchiverAuth";
-        options.Cookie.HttpOnly = true;
-        options.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
-        options.Cookie.SameSite = SameSiteMode.Strict;
-        options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
-        options.SlidingExpiration = true;
-    });
-
-// conditional OAuth setup
-var oauthOptions = builder.Configuration.GetSection(OAuthOptions.OAuth).Get<OAuthOptions>();
-if (oauthOptions?.Enabled ?? false)
-{
-    authBuilder.AddCookie("OidcCookie"); // temporary storage for OIDC result
-    authBuilder.AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, (o) => { 
-        o.ClientId = oauthOptions.ClientId;
-        o.ClientSecret = oauthOptions.ClientSecret;
-        o.CallbackPath = "/oidc-signin-completed";
-        o.Authority = oauthOptions.Authority;
-        o.ResponseType = OpenIdConnectResponseType.Code;
-        o.GetClaimsFromUserInfoEndpoint = true;
-        o.SignInScheme = "OidcCookie";
-        o.TokenValidationParameters.NameClaimType = "name";
-        if (oauthOptions.ClientScopes != null) { 
-            o.Scope.Clear();
-            foreach (var scope in oauthOptions.ClientScopes)
-            {
-                o.Scope.Add(scope);
-            }
-        }
-        o.Events.OnUserInformationReceived = async (UserInformationReceivedContext ctx) => {
-            var handler = ctx.Request.HttpContext.RequestServices.GetRequiredService<AuthenticationHandler>();
-            
-            var email = ctx.User.RootElement.GetProperty("email").GetString();
-            var id = ctx.User.RootElement.GetProperty("sub").GetString();
-            var name = ctx.User.RootElement.GetProperty("name").GetString();
-
-            var identity = ctx.Principal.Identity as ClaimsIdentity;
-            identity.AddClaim(new Claim(ClaimTypes.Email, email));
-            identity.AddClaim(new Claim(ClaimTypes.Name, name));
-            
-            await handler.HandleUserAuthenticated(
-                OpenIdConnectDefaults.AuthenticationScheme
-                , id
-                , persistAuthentication: false
-                , remoteIdentity: ctx.Principal.Identity);
-            
-        };
-    });
-}
-
+builder.AddAuth();
 
 // Set global encoding to UTF-8
 System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
@@ -467,10 +401,7 @@ app.UseSession();
 // Add Rate Limiting Middleware
 app.UseRateLimiter();
 
-// Add our custom authentication middleware
-app.UseMiddleware<AuthenticationMiddleware>();
-
-app.UseAuthorization();
+app.UseAuth();
 
 app.MapControllerRoute(
     name: "default",
